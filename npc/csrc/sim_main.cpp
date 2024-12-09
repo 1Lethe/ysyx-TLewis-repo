3ae539d4a2fa6b96f27581cb4212b36dac4551f6
tracer-ysyx <tracer@ysyx.org>
2024-12-09 15:12:34 +0800

    > sim RTL ysyx_22040000 杨羽童 Linux tonglewis-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:12:34 up  2:29,  4 users,  load average: 0.89, 0.51, 0.43

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -5,24 +5,26 @@
 #include <stdlib.h>                                                    
 #include <assert.h>                   
 
+#define SIM_MODULE_NAME RandomGen;
+
 VerilatedContext* contextp = NULL;
 VerilatedFstC* tfp = NULL;
-Vtop* top;
+Vtop* SIM_MODULE_NAME;
 
-int sim_time = 50;
+int sim_time = 500;
 
 void sim_init(int argc, char** argv){
     contextp = new VerilatedContext;                 
     contextp->commandArgs(argc, argv);                                 
-    top = new Vtop{contextp};                                    
+    SIM_MODULE_NAME = new Vtop{contextp};                                    
                                                                        
     tfp = new VerilatedFstC;                            
     contextp->traceEverOn(true);                                       
-    top->trace(tfp, 99);  // Trace 99 levels of hierarchy (or see below)
+    SIM_MODULE_NAME->trace(tfp, 99);  // Trace 99 levels of hierarchy (or see below)
     tfp->open("wave/wave.fst");                                             
 }
 
-void dump_wave(void){
+void dump_wave(Vtop *top){
     top->eval();
     tfp->dump(contextp->time());                  
     contextp->timeInc(1);                         
@@ -46,29 +48,8 @@ int main(int argc, char** argv) {
     
     
     while(!contextp->gotFinish() && sim_time >= 0){   
-            
-        top->alu_command = 0;top->inA = 1;top->inB = 1;dump_wave();
-        top->inA = -1;top->inB = -1;dump_wave(); 
-        top->inA = -7;top->inB = -7;dump_wave();
-
-        top->alu_command = 1;top->inA = 1;top->inB = 1;dump_wave();
-        top->inA = -7;top->inB = 7;dump_wave(); 
-        top->inA = 7;top->inB = 1;dump_wave();
-
-        top->alu_command = 2;top->inA = 7;top->inB = 0;dump_wave();
-        top->alu_command = 3;top->inA = 3;top->inB = 5;dump_wave();
-        top->alu_command = 4;top->inA = 4;top->inB = -3;dump_wave();
-        top->alu_command = 5;top->inA = -7;top->inB = -1;dump_wave();
-
-        top->alu_command = 6;top->inA = 7;top->inB = 5;dump_wave();
-        top->inA = 7;top->inB = -5;dump_wave();
-        top->inA = -7;top->inB = 7;dump_wave();
-        top->inA = -5;top->inB = -7;dump_wave();
-        top->inA = 1;top->inB = -5;dump_wave();
-        top->inA = -1;top->inB = -1;dump_wave();
-
-        top->alu_command = 7;top->inA = 3;top->inB = 3;dump_wave();
-        top->inA = -7;top->inB = 3;dump_wave();
+        single_cycle(SIM_MODULE_NAME);
+        dump_wave(SIM_MODULE_NAME);
     }   
     tfp->close();                                     
     return 0;                                         
