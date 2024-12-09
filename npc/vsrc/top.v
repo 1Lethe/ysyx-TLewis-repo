0f7f255b34cda04d6739fe5be5d84f175864cb1f
tracer-ysyx <tracer@ysyx.org>
2024-12-09 21:02:54 +0800

    > sim RTL ysyx_22040000 杨羽童 Linux tonglewis-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:02:54 up  8:19,  4 users,  load average: 1.43, 1.06, 0.91

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -9,13 +9,13 @@ module top(
 wire [7:0] random_output;
 
 RandomGen RandomGen(
-    .clk(clk),
+    .clk(buttom),
     .rst(rst),
     .RandomGen_output(random_output)
 );
 
 segs segs(
-    .clk(clk),
+    .clk(buttom),
     .rst(rst),
     .segs_input(random_output),
     .seg0_output(seg0),
