b1bfbbc83a54c5605da62e3b4bf58fd1aa58471e
Tlewis <farawaylethe@163.com>
2024-12-09 22:06:38 +0800

    sim_main.cpp format fix

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -1,9 +1,9 @@
-#include "Vtop.h"                                                      
-#include "verilated.h"                                                 
-#include "verilated_fst_c.h"                                           
-#include <stdio.h>                                                     
-#include <stdlib.h>                                                    
-#include <assert.h>                   
+#include "Vtop.h"
+#include "verilated.h"
+#include "verilated_fst_c.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
 //Still need to change SIM_TOPNAME in makefile!
 #define SIM_MODULE_NAME top
@@ -11,25 +11,23 @@
 VerilatedContext* contextp = NULL;
 VerilatedFstC* tfp = NULL;
 Vtop* top;
-
 int sim_time = 50;
-
 void sim_init(int argc, char** argv){
-    contextp = new VerilatedContext;                 
-    contextp->commandArgs(argc, argv);                                 
-    SIM_MODULE_NAME = new Vtop{contextp};                                    
-                                                                       
-    tfp = new VerilatedFstC;                            
-    contextp->traceEverOn(true);                                       
+    contextp = new VerilatedContext;
+    contextp->commandArgs(argc, argv);
+    SIM_MODULE_NAME = new Vtop{contextp};
+
+    tfp = new VerilatedFstC;
+    contextp->traceEverOn(true);
     SIM_MODULE_NAME->trace(tfp, 99);  // Trace 99 levels of hierarchy (or see below)
-    tfp->open("wave/wave.fst");                                             
+    tfp->open("wave/wave.fst");
 }
 
-void dump_wave(void){
-    SIM_MODULE_NAME->eval();
-    tfp->dump(contextp->time());                  
-    contextp->timeInc(1);                         
-    sim_time--;                                   
+void dump_wave(Vtop* top){
+    top->eval();
+    tfp->dump(contextp->time());
+    contextp->timeInc(1);
+    sim_time--;
 }
 
 void single_cycle(Vtop* top){
@@ -46,13 +44,11 @@ void reset(Vtop* top, int n){
 int main(int argc, char** argv) {                                      
     
     sim_init(argc, argv);
-
-    reset(SIM_MODULE_NAME,10);                                                 
-    
+    reset(SIM_MODULE_NAME,10);
     while(!contextp->gotFinish() && sim_time >= 0){   
         single_cycle(SIM_MODULE_NAME);
-        dump_wave();
+        dump_wave(SIM_MODULE_NAME);
     }   
-    tfp->close();                                     
-    return 0;                                         
+    tfp->close();
+    return 0;
 }                                                     
