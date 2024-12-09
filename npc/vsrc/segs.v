429e20a7acee83fd1ed1cb28f693dcff7152237c
tracer-ysyx <tracer@ysyx.org>
2024-12-09 21:30:13 +0800

    > sim RTL ysyx_22040000 杨羽童 Linux tonglewis-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:30:13 up  8:47,  4 users,  load average: 0.53, 0.56, 0.66

--- npc/vsrc/segs.v
+++ npc/vsrc/segs.v
@@ -5,7 +5,7 @@ module segs(
 );
 
 wire [7:0] segs [15:0];
-assign segs[0] = 8'b11111101;
+assign segs[0] = 8'b11111100;
 assign segs[1] = 8'b01100000;
 assign segs[2] = 8'b11011010;
 assign segs[3] = 8'b11110010;
@@ -13,8 +13,8 @@ assign segs[4] = 8'b01100110;
 assign segs[5] = 8'b10110110;
 assign segs[6] = 8'b10111110;
 assign segs[7] = 8'b11100000;
-assign segs[8] = 8'b11111111;
-assign segs[9] = 8'b11110111;
+assign segs[8] = 8'b11111110;
+assign segs[9] = 8'b11110110;
 assign segs[10] = 8'b11101100;
 assign segs[11] = 8'b00111110;
 assign segs[12] = 8'b10011100;
