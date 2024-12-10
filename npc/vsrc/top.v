7a5aea677e7b44a9cfdadf6c112e0cbbfafbfcd9
tracer-ysyx <tracer@ysyx.org>
2024-12-10 20:08:21 +0800

    > sim RTL ysyx_22040000 杨羽童 Linux tonglewis-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:08:21 up  7:37,  3 users,  load average: 1.64, 1.09, 0.76

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -3,37 +3,24 @@ module top(
     input rst,
     input ps2_clk,
     input ps2_data,
-    input ps2data_get_p,
-    output ready,
     output overflow
 );
 
-reg nextdata_n;
-reg datarec_flag;
-reg[7:0] dataget;
 wire ps2_ready;
 wire [7:0] data;
+wire nextdata_n
 
-assign nextdata_n = ~ready;
-
-always @(*) begin
-    if(datarec_flag) begin
-        $display("receive: %x",dataget[7:0]);
-    end
-end
-
-always @(posedge clk or negedge rst) begin
-    if(rst) begin 
-        dataget <= 8'b0;
-        datarec_flag <= 1'b0;
-    end else if(ready) begin
-        dataget <= data;
-        datarec_flag <= 1'b1;
-    end else begin
-        datarec_flag <= 1'b0;
-    end
-end
 assign ready = ps2_ready;
+
+keyboard_read keyboard_read(
+    .clk(clk),
+    .rst(rst),
+    .ps2read_data(data),
+    .ps2read_ready(ps2_ready),
+    .ps2read_nextdata(),
+    .ps2read_dataget(),
+    .ps2read_datarec()
+);
 
 ps2_keyboard ps2_keyboard(
     .clk(clk),
