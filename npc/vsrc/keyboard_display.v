6696ada75d3002652f8df0f577631469b28bf5fd
tracer-ysyx <tracer@ysyx.org>
2024-12-11 16:27:02 +0800

    > sim RTL ysyx_22040000 杨羽童 Linux tonglewis-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:27:02 up  1:27,  6 users,  load average: 0.26, 0.22, 0.30

--- npc/vsrc/keyboard_display.v
+++ npc/vsrc/keyboard_display.v
@@ -20,8 +20,8 @@ reg[3:0] kb_state;
 
 assign segs_enable = kb_state == MAKE ? 1'b1 : 1'b0;
 
-always @(posedge clk or negedge rst) begin
-    if(rst == 1'b0) begin
+always @(posedge clk) begin
+    if(rst) begin
         kb_state <= IDLE;
     end else
         case(kb_state)
@@ -55,16 +55,16 @@ always @(posedge clk or negedge rst) begin
         endcase
 end
 
-always @(posedge clk or negedge rst) begin
-    if(rst == 1'b0) begin
+always @(posedge clk) begin
+    if(rst) begin
         ps2dis_seg0_1 <= 8'b0;
     end else if(kb_state == MAKE) begin
         ps2dis_seg0_1 <= ps2dis_data;
     end
 end
 
-always @(posedge clk or negedge rst) begin
-    if(rst == 1'b0) begin
+always @(posedge clk) begin
+    if(rst) begin
         ps2dis_seg2_3 <= 8'b0;
     end else if((kb_state == MAKE) && (shift_flag == 1'b0)) begin
         case(ps2dis_data) //ASCII 
@@ -150,16 +150,16 @@ always @(posedge clk or negedge rst) begin
     end
 end
 
-always @(posedge clk or negedge rst) begin
-    if(rst == 1'b0) begin
+always @(posedge clk) begin
+    if(rst) begin
         keytime_cnt <= 8'b0;
     end else if((ps2dis_recFlag == 1'b1) && (ps2dis_data == 8'hF0)) begin
         keytime_cnt <= keytime_cnt + 1'b1;
     end
 end
 
-always @(posedge clk or negedge rst) begin
-    if(rst == 1'b0) begin
+always @(posedge clk) begin
+    if(rst) begin
         shift_flag <= 1'b0;
     end else if((ps2dis_recFlag == 1'b1) && (ps2dis_data == 8'h12)) begin
         shift_flag <= 1'b1;
@@ -168,8 +168,8 @@ always @(posedge clk or negedge rst) begin
     end        
 end
 
-always @(posedge clk or negedge rst) begin
-    if(rst == 1'b0) begin
+always @(posedge clk) begin
+    if(rst) begin
         ctrl_flag <= 1'b0;
     end else if((ps2dis_recFlag == 1'b1) && (ps2dis_data == 8'h14)) begin
         ctrl_flag <= 1'b1;
