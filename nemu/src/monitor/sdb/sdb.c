ec3a57e47025b578dcba92eb10889fc883a5bcfe
tracer-ysyx <tracer@ysyx.org>
2024-12-15 18:24:20 +0800

    > compile NEMU ysyx_22040000 杨羽童 Linux tonglewis-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:24:20 up  7:50,  2 users,  load average: 0.28, 0.25, 0.36

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -93,17 +93,21 @@ static int cmd_x(char *args){
 
   if(args == NULL){
     printf("Command x need args.\n");
-  }else{
-    if(sscanf(args, "%d %x", &scan_num, &mem_start_place) == 2){
-      for(int i = 0;i < scan_num;i++){
-        pmem_scan = guest_to_host(mem_start_place + i);
-        printf("0x%x = 0x%x\n", mem_start_place+i, *pmem_scan);
-      }
-    }else{
-      printf("Invalid input for x command.\n");
-    }
+    return 0;
   }
 
+  if(sscanf(args, "%d %x", &scan_num, &mem_start_place) == 2){
+    if(scan_num <= 0){
+      printf("Invalid scan_num input.This Arg should > 0.");
+      return 0;
+    }
+    for(int i = 0;i < scan_num;i++){
+      pmem_scan = guest_to_host(mem_start_place + i);
+      printf("0x%x = 0x%x\n", mem_start_place+i, *pmem_scan);
+    }
+  }else{
+    printf("Invalid x command input.");
+  }
   return 0;
 }
 
