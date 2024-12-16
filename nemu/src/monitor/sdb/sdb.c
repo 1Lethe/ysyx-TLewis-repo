636ff6e26d21878567f7d582bffcb762b741ebd2
tracer-ysyx <tracer@ysyx.org>
2024-12-16 20:19:22 +0800

    > compile NEMU ysyx_22040000 杨羽童 Linux tonglewis-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:19:22 up  9:22,  8 users,  load average: 1.43, 0.76, 0.38

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -207,8 +207,8 @@ void init_sdb() {
   /* Initialize the watchpoint pool. */
   init_wp_pool();
 
-  expr("((1-2+3)-(3+2)+(1+1))", NULL);
-  expr("(2*9/9)", NULL);
-  expr("(3+4*(6-2) /2+ (2-  1*1 *(1*2)))", NULL);
+  //expr("((1-2+3)-(3+2)+(1+1))", NULL);
+  expr("(2*9/ 9)", NULL);
+  //expr("(3+4*(6-2) /2+ (2-  1*1 *(1*2)))", NULL);
 
 }
