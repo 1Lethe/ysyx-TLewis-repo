96d98eac76944c6b83294cd1bab2e0ea014cc80f
tracer-ysyx <tracer@ysyx.org>
2024-12-22 17:11:57 +0800

    > compile NEMU ysyx_22040000 杨羽童 Linux tonglewis-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:11:57 up 14:09,  2 users,  load average: 0.35, 0.26, 0.29

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -42,8 +42,10 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 
   /* Trace watchpoint */
+#ifdef CONFIG_WATCHPOINT
   bool is_stop = trace_wp();
   if(is_stop) nemu_state.state = NEMU_STOP;
+#endif
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
