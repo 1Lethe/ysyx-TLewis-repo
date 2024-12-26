ef691e81b060e76491b76e1d01de31de5ef93cb5
Tlewis <farawaylethe@163.com>
2024-12-26 13:53:17 +0800

    NPC:feat(vsrc): 添加随机数生成和算术逻辑单元模块

---
+++ npc/vsrc/RandomGen.v
@@ -0,0 +1,32 @@
+module RandomGen (
+    input clk,
+    input buttom,
+    input rst,
+    output wire [7:0]RandomGen_output
+);
+
+reg buttom_reg1;
+wire buttom_posedge;
+reg[7:0] shift_reg;
+
+always @(posedge clk or negedge rst) begin
+    if(rst) buttom_reg1 <= 1'b0;
+    else begin
+        buttom_reg1 <= buttom;
+    end
+end
+
+assign buttom_posedge = buttom & (~buttom_reg1); 
+
+always @(posedge clk or negedge rst) begin
+    if(rst) shift_reg <= 8'b11111111;
+    else if(buttom_posedge == 1'b1) begin
+        shift_reg <= {shift_reg[4]^shift_reg[3]^shift_reg[2]^shift_reg[0],shift_reg[7:1]};
+    end
+    else
+        shift_reg <= shift_reg;
+end
+
+assign RandomGen_output = shift_reg; 
+
+endmodule
