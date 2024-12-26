e93c12c291941afa0a215069ec111391685e475e
tracer-ysyx <tracer@ysyx.org>
2024-12-26 14:00:21 +0800

    > sim RTL ysyx_22040000 杨羽童 Linux tonglewis-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:00:21 up  1:47,  3 users,  load average: 0.18, 0.59, 0.47

--- nemu/src/isa/riscv32/local-include/reg.h
+++ nemu/src/isa/riscv32/local-include/reg.h
@@ -22,7 +22,6 @@ static inline int check_reg_idx(int idx) {
   IFDEF(CONFIG_RT_CHECK, assert(idx >= 0 && idx < MUXDEF(CONFIG_RVE, 16, 32)));
   return idx;
 }
-void isa_reg_display();
 
 #define gpr(idx) (cpu.gpr[check_reg_idx(idx)])
 
--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -19,7 +19,6 @@
 #include <time.h>
 #include <assert.h>
 #include <string.h>
-#include <stdbool.h>
 
 // this should be enough
 static char buf[65536] = {};
@@ -32,57 +31,8 @@ static char *code_format =
 "  return 0; "
 "}";
 
-//FIXME: still cannot filter expression that divide by zero!!!!
-
-bool start_gen_flag = 0;
-int depth = 0;
-
-static uint32_t choose(uint32_t n){
-  return (rand() % n);
-}
-
-static int gen_num(void){
-  char num_buf[20];
-  sprintf(num_buf, "%d", (uint32_t)choose(1000));
-  strncat(buf, num_buf, 20);
-  return 0;
-}
-
-static int gen(char x){
-  strncat(buf, &x, 1);
-  return 0;
-}
-
-static int gen_rand_op(void){
-  char rand_op;
-  switch (choose(4)){
-    case 0 : rand_op = '+';break;
-    case 1 : rand_op = '-';break;
-    case 2 : rand_op = '*';break;
-    case 3 : rand_op = '/';break;
-    default : assert(0);
-  }
-  strncat(buf, &rand_op, 1);
-  return 0;
-}
-
 static void gen_rand_expr() {
-  if((strlen(buf) >= sizeof(buf)/sizeof(buf[0]) - 20) || depth >= 20){
-    gen_num();
-    return;
-  }else{
-    depth++;
-    switch (choose(4))
-    {
-      case 0 : gen_num();start_gen_flag = true;break;
-      case 1 : gen('('); gen_rand_expr(); gen(')');start_gen_flag = true; break;
-      case 2 : gen_rand_expr(); gen_rand_op(); gen_rand_expr();start_gen_flag = true;break;
-      case 3 : if(start_gen_flag) gen(' '); gen_rand_expr();break;
-      default:
-        break;
-    }
-  depth--;
-  }
+  buf[0] = '\0';
 }
 
 int main(int argc, char *argv[]) {
@@ -94,9 +44,7 @@ int main(int argc, char *argv[]) {
   }
   int i;
   for (i = 0; i < loop; i ++) {
-    memset(buf,'\0',sizeof(buf)/sizeof(buf[0]));
     gen_rand_expr();
-    start_gen_flag = false;
 
     sprintf(code_buf, code_format, buf);
 
@@ -105,7 +53,7 @@ int main(int argc, char *argv[]) {
     fputs(code_buf, fp);
     fclose(fp);
 
-    int ret = system("gcc -w /tmp/.code.c -o /tmp/.expr");
+    int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
     if (ret != 0) continue;
 
     fp = popen("/tmp/.expr", "r");
