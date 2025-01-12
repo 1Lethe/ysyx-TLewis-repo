760628f4188694f7190d44d342d9daa361a4ead8
Tlewis <farawaylethe@163.com>
2025-01-12 22:08:56 +0800

    PA2 update sdb mem scan

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -107,10 +107,23 @@ static int cmd_x(char *args){
       printf("physical memory area [" FMT_PADDR ", " FMT_PADDR "]\n", PMEM_LEFT, PMEM_RIGHT);
       return 0;
     }
+
+    int base_add = mem_start_place;
     for(int i = 0;i < scan_num;i++){
-      pmem_scan = guest_to_host(mem_start_place + i);
-      printf("0x%08x = 0x%02x\n", mem_start_place+i, *pmem_scan);
+      int offset = i % 4;
+      if(offset == 0){
+        if(i == 0){
+          printf("0x%08x : ", mem_start_place);
+        }else{
+          printf("\n");
+          printf("0x%08x : ", mem_start_place + i);
+          base_add += 0x4;
+        }
+      }
+      pmem_scan = guest_to_host(base_add + 3 - offset);
+      printf("%02x", *pmem_scan);
     }
+    printf("\n");
   }else{
     printf("Invalid x command input.\n");
   }
