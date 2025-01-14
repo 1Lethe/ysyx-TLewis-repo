dfe923c44574cc3f4830c3e3e2839d16a7ccd591
tracer-ysyx <tracer@ysyx.org>
2025-01-15 01:42:08 +0800

    > sim RTL ysyx_22040000 杨羽童 Linux tonglewis-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:42:08 up 11:22,  2 users,  load average: 0.45, 0.39, 0.60

--- npc/csrc/npcsrc/memory.h
+++ npc/csrc/npcsrc/memory.h
@@ -11,7 +11,9 @@ uint32_t pmem[] = {
 
 uint32_t* guest_to_host(uint32_t paddr);
 uint32_t host_to_guest(uint32_t *haddr);
-uint32_t pmem_read(uint32_t addr);
+uint32_t pmem_read(uint32_t addr){
+    return 0;
+}
 
 
 #endif
