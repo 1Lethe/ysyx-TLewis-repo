a1905abe82b06abf038ed26fc949a8091043d4e0
tracer-ysyx <tracer@ysyx.org>
2025-01-15 01:27:58 +0800

    > sim RTL ysyx_22040000 杨羽童 Linux tonglewis-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:27:58 up 11:08,  2 users,  load average: 0.89, 1.41, 1.02

---
+++ npc/csrc/npcsrc/memory.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "Vysyx_24120013_top.h"
+#include "verilated.h"
+#include "memory.h"
+
+uint32_t* guest_to_host(uint32_t paddr) { return pmem + paddr - RESET_VECTOR; }
+uint32_t host_to_guest(uint32_t *haddr) { return haddr - pmem + RESET_VECTOR; }
+
+uint32_t pmem_read(int addr){
+    return *(guest_to_host(addr));
+}
---
+++ npc/csrc/npcsrc/memory.h
@@ -0,0 +1,12 @@
+#ifndef __MEMORY_H__
+#define __MEMORY_H__
+
+#include <stdint.h>
+
+#define RESET_VECTOR 0x80000000
+
+uint32_t pmem[] = {
+    
+};
+
+#endif
