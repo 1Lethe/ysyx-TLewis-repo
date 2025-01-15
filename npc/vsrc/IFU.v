872866efde481c5cd4183385ddfe182c97163dd7
tracer-ysyx <tracer@ysyx.org>
2025-01-15 17:44:12 +0800

    > sim RTL ysyx_22040000 杨羽童 Linux tonglewis-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:44:12 up  5:21,  3 users,  load average: 0.35, 0.68, 0.64

--- npc/vsrc/IFU.v
+++ npc/vsrc/IFU.v
@@ -2,9 +2,14 @@ module ysyx_24120023_IFU (
     input clk,
     input rst,
     input [31:0] inst,
-    output wire [31:0] IFU_inst
+    output reg [31:0] IFU_inst
 );
 
-assign IFU_inst = inst;
+always @(posedge clk) begin
+    if(rst)
+        IFU_inst <= 32'b0;
+    else
+        IFU_inst <= inst;
+end
 
 endmodule
