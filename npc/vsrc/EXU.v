383e08cc37b2596d851d9eedfd0091dd14a40cf9
tracer-ysyx <tracer@ysyx.org>
2025-01-15 16:47:26 +0800

    > sim RTL ysyx_22040000 杨羽童 Linux tonglewis-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:47:26 up  4:24,  3 users,  load average: 0.31, 0.32, 0.36

--- npc/vsrc/EXU.v
+++ npc/vsrc/EXU.v
@@ -17,9 +17,9 @@ module ysyx_24120013_EXU #(ADDR_WIDTH = 32, DATA_WIDTH = 32)(
     always @(*) begin
         case (command)
             2'b01 :
-                EXU_wdata <= src1 + imm;
+                EXU_wdata = src1 + imm;
             default :
-                EXU_wdata <= {DATA_WIDTH{1'b0}};
+                EXU_wdata = {DATA_WIDTH{1'b0}};
         endcase
     end
 
