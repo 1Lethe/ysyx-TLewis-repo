60faba6cfe682c1ee001d829815bac2bed90fbae
tracer-ysyx <tracer@ysyx.org>
2025-01-16 20:43:25 +0800

    > sim RTL ysyx_22040000 杨羽童 Linux tonglewis-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:43:25 up 12:14,  4 users,  load average: 0.44, 0.56, 0.51

--- npc/vsrc/IDU.v
+++ npc/vsrc/IDU.v
@@ -57,7 +57,7 @@ module ysyx_24120013_IDU #(COMMAND_WIDTH = 2, ADDR_WIDTH = 5, DATA_WIDTH = 32)(
             7'b0010011 :
                 IDU_command = 2'b01;
             default :
-                IDU_command = 2'b00;
+                halt;
         endcase
     end
 
