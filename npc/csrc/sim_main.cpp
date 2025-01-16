5674ea77885e41781621ec944a331c2faed62ec7
tracer-ysyx <tracer@ysyx.org>
2025-01-16 21:33:27 +0800

    > sim RTL ysyx_22040000 杨羽童 Linux tonglewis-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:33:27 up 13:04,  4 users,  load average: 0.51, 0.66, 0.62

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -63,7 +63,7 @@ int main(int argc, char** argv) {
     
     sim_init(argc, argv);
 
-    reset(SIM_MODULE_NAME, 1);
+    reset(SIM_MODULE_NAME, 10);
 
 #ifdef USE_TESTBENCH
     while(!contextp->gotFinish()){   
