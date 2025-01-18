e29ce55064df80c87f7901380796d66d917b5884
tracer-ysyx <tracer@ysyx.org>
2025-01-18 17:35:35 +0800

    > compile NEMU ysyx_22040000 杨羽童 Linux tonglewis-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:35:35 up  7:37,  2 users,  load average: 1.53, 0.80, 0.57

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -33,9 +33,7 @@ char *strcpy(char *dst, const char *src) {
 
   char *original_dst = dst;
   for(int i = 0; i <= srclen; i++){
-    *dst = *src;
-    dst++;
-    src++;
+    *dst++ = *src++;
   }
 
   return original_dst;
@@ -47,49 +45,141 @@ char *strncpy(char *dst, const char *src, size_t n) {
   }
 
   if((dst >= src && dst < src + n) || (dst <= src && src < dst + n)){
-    panic("dst and src that need to cpy overlap.");
+    panic("dst and src that need to copy overlap.");
   }
 
   char *original_dst = dst;
   size_t i = 0;
   for(i = 0; i < n && *src != '\0'; i++){
-    *dst = *src;
-    dst++;src++;
+    *dst++ = *src++;
   }
   for( ; i < n; i++){
-    *dst = '\0';
-    dst++;
+    *dst++ = '\0';
   }
 
   return original_dst;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  if(dst == NULL || src == NULL){
+    panic("dst or src is NULL.");
+  }
+
+  size_t srclen = strlen(src);
+  size_t dstlen = strlen(dst);
+  if((dst >= src && dst < src + srclen + 1) || (src >= dst && src < dst + dstlen + 1)){
+    panic("dst and src overlap.");
+  }
+
+  char *original_dst = dst;
+  dst += dstlen;
+  while(*src != '\0'){
+    *dst++ = *src++;
+  }
+  *dst++ = '\0';
+
+  return original_dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  if(s1 == NULL || s2 == NULL){
+    panic("s1 or s2 is NULL.");
+  }
+
+  while(*s1 != '\0' && *s2 != '\0'){
+    if((unsigned char)*s1 != (unsigned char) *s2){
+      return (unsigned char)*s1 - (unsigned char)*s2;
+    }
+    s1++;s2++;
+  }
+
+  return (unsigned char)*s1 - (unsigned char)*s2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  if(s1 == NULL || s2 == NULL){
+    panic("s1 or s2 is NULL.");
+  }
+
+  for(size_t i = 0; i < n && (*s1 != '\0' && (*s2 != '\0')); i++){
+    if((unsigned char)*s1 != (unsigned char)*s2){
+      return (unsigned char)*s1 - (unsigned char)*s2;
+    }
+    s1++;s2++;
+  }
+
+  return 0;
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  if(s == NULL){
+    panic("s is NULL.");
+  }
+
+  unsigned char *ptr = (unsigned char *)s;
+  for(size_t i = 0; i < n; i++){
+    *ptr++ = (unsigned char)c;
+  }
+
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+  if(dst == NULL || src == NULL){
+    panic("dst or src is NULL.");
+  }
+
+  unsigned char *temp = (unsigned char *)malloc(n);
+  if(temp == NULL){
+    panic("failed to malloc.");
+  }
+  unsigned char *original_temp = temp;
+  unsigned char *ptr_src = (unsigned char *)src;
+  unsigned char *ptr_dst = (unsigned char *)dst;
+  for(size_t i = 0; i < n; i++){
+    *temp++ = *ptr_src++;
+  }
+  for(size_t i = 0; i < n; i++){
+    *ptr_dst++ = *temp++;
+  }
+
+  free(original_temp);
+
+  return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  if(out == NULL || in == NULL){
+    panic("out or in is NULL.");
+  }
+
+  if((in < out && out < in + n) || (in > out && in < out + n)){
+    panic("out and in overlap.");
+  }
+
+  unsigned char *ptr_out = (unsigned char *)out;
+  unsigned char *ptr_in = (unsigned char *)in;
+  for(size_t i = 0; i < n; i++){
+    *ptr_out++ = *ptr_in++;
+  }
+
+  return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  if(s1 == NULL || s2 == NULL){
+    panic("s1 or s2 is NULL.");
+  }
+
+  unsigned char *s1_ptr = (unsigned char *)s1;
+  unsigned char *s2_ptr = (unsigned char *)s2;
+  for (size_t i = 0; i < n; i++){
+    if(*s1_ptr != *s2_ptr){
+      return *s1_ptr - *s2_ptr;
+    }
+  }
+
+  return 0;
 }
 
 #endif
