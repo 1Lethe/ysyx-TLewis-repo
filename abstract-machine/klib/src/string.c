549192ba8fca1256c3a4e8e1c388b054beaeabd8
tracer-ysyx <tracer@ysyx.org>
2025-01-18 15:43:15 +0800

    > compile NEMU ysyx_22040000 杨羽童 Linux tonglewis-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:43:15 up  5:44,  2 users,  load average: 0.80, 0.75, 0.62

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -6,7 +6,12 @@
 
 size_t strlen(const char *s) {
   size_t len = 0;
-  for(int i = 0; ;i++){
+
+  if(s == NULL){
+    panic("s is NULL.");
+  }
+
+  for(int i = 0; ; i++){
     if(*s != '\0'){
       len++;
       s++;
@@ -17,11 +22,46 @@ size_t strlen(const char *s) {
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  if(dst == NULL || src == NULL){
+    panic("dst or src is NULL.");
+  }
+
+  size_t srclen = strlen(src);
+  if((dst >= src && dst < src + srclen + 1) || (dst <= src && src < dst + srclen + 1)){
+    panic("dst and src overlap.");
+  }
+
+  char *original_dst = dst;
+  for(int i = 0; i <= srclen; i++){
+    *dst = *src;
+    dst++;
+    src++;
+  }
+
+  return original_dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  if(dst == NULL || src == NULL){
+    panic("dst or src is NULL.");
+  }
+
+  if((dst >= src && dst < src + n) || (dst <= src && src < dst + n)){
+    panic("dst and src that need to cpy overlap.");
+  }
+
+  char *original_dst = dst;
+  size_t i = 0;
+  for(i = 0; i < n && *src != '\0'; i++){
+    *dst = *src;
+    dst++;src++;
+  }
+  for( ; i < n; i++){
+    *dst = '\0';
+    dst++;
+  }
+
+  return original_dst;
 }
 
 char *strcat(char *dst, const char *src) {
