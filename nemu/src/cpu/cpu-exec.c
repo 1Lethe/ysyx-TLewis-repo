1f5022506f381ccf0c8dd430fbbe5e25a6f9e3df
tracer-ysyx <tracer@ysyx.org>
2025-01-19 17:56:11 +0800

    > compile NEMU ysyx_22040000 杨羽童 Linux tonglewis-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:56:11 up  5:37,  2 users,  load average: 4.88, 5.20, 5.16

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -99,7 +99,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
     free(iringbuf[i]);
   }
   char *instbuf = (char *)malloc(32*sizeof(char));
-  memset(instbuf, '0', 32*sizeof(char));
+  memset(instbuf, '\0', 32*sizeof(char));
   memcpy(instbuf, s->logbuf, 32*sizeof(char));
   iringbuf[iring_index++] = instbuf;
   for(int i = 0; i < IRING_BUF_SIZE; i++){
@@ -149,7 +149,9 @@ void cpu_exec(uint64_t n) {
   }
 
   uint64_t timer_start = get_time();
-  memset(iringbuf, '\0', sizeof(iringbuf)); // Init iringbuf
+  for(int i = 0; i < IRING_BUF_SIZE; i++){
+    iringbuf[i] = NULL;
+  }
 
   execute(n);// execute command
 
