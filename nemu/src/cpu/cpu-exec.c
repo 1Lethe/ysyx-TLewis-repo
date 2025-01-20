36d7221157a52ddbb7bace20c27171e36fdf45b6
tracer-ysyx <tracer@ysyx.org>
2025-01-21 03:40:39 +0800

    > compile NEMU ysyx_22040000 杨羽童 Linux tonglewis-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:40:39 up  3:15,  3 users,  load average: 0.79, 0.69, 0.61

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -207,10 +207,13 @@ static void ftrace(Decode *s){
         }else{
           if(funcall_value_stack[funcall_time - 1] == sym_value_prev && \
               funcall_value_stack[funcall_time - 2] == sym_value){
+            for(int i = 0;i < funcall_time - 1; i++) printf("  ");
+            printf("ret : ");
             funcall_value_stack[funcall_time - 1] = 0;
             funcall_time--;
             //printf("%x %x %x %x %d | ", funcall_value_stack[0], funcall_value_stack[1], funcall_value_stack[2], funcall_value_stack[3], funcall_time);
           }else{
+            for(int i = 0;i < funcall_time - 1; i++) printf("  ");
             printf("call : ");
             funcall_value_stack[funcall_time] = sym_value;
             funcall_time++;
@@ -218,7 +221,6 @@ static void ftrace(Decode *s){
           }
         }
 
-        for(int i = 0;i < funcall_time - 1; i++) printf("  ");
 
         char str_buf;
         char str[20];
