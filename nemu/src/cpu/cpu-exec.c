7153b7ec7e05ff50d61e67904a42b107998b1636
tracer-ysyx <tracer@ysyx.org>
2025-01-21 03:59:18 +0800

    > compile NEMU ysyx_22040000 杨羽童 Linux tonglewis-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:59:18 up  3:33,  3 users,  load average: 0.19, 0.29, 0.35

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -196,22 +196,24 @@ static void ftrace(Decode *s){
       char str_buf;
       char str[20];
       char *str_ptr = str;
+
       sym_value_prev = sym_value;
       sym_value = elf_sym[i].st_value;
       if(sym_value != sym_value_prev){
         /* call function or return from function */
         printf("0x%x: ", pc);
         memset(str, '\0', 20);
+        
         while((str_buf = fgetc(fp)) != EOF){
           *str_ptr++ = str_buf;
           if(str_buf == '\0') break;
         }
+        
         /* maintain a stack which contain the value of fun in symbol table */
         if(sym_value == 0x80000000){
           printf("call");
           funcall_value_stack[funcall_time] = sym_value;
           funcall_time++;
-          //printf("%x %x %x %x %d | ", funcall_value_stack[0], funcall_value_stack[1], funcall_value_stack[2], funcall_value_stack[3], funcall_time);
         }else{
           if(funcall_value_stack[funcall_time - 1] == sym_value_prev && \
               funcall_value_stack[funcall_time - 2] == sym_value){
@@ -219,7 +221,6 @@ static void ftrace(Decode *s){
             for(int i = 0;i < funcall_time - 1; i++) printf("  ");
             printf("ret");
             funcall_time--;
-            //printf("%x %x %x %x %d | ", funcall_value_stack[0], funcall_value_stack[1], funcall_value_stack[2], funcall_value_stack[3], funcall_time);
           }else{
             funcall_value_stack[funcall_time] = sym_value;
             funcall_time++;
@@ -227,7 +228,6 @@ static void ftrace(Decode *s){
             printf("call");
 
             printf("[%s@0x%x]\n", str, elf_sym[i].st_value);
-            //printf("%x %x %x %x %d | ", funcall_value_stack[0], funcall_value_stack[1], funcall_value_stack[2], funcall_value_stack[3], funcall_time);
           }
         }
         break;
