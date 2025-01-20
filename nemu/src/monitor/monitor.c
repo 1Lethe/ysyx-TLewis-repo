19e81cfd39eee65c37fc1bf9a79d894f4ea28dc9
tracer-ysyx <tracer@ysyx.org>
2025-01-20 17:21:21 +0800

    > compile NEMU ysyx_22040000 杨羽童 Linux tonglewis-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:21:21 up  2:34,  3 users,  load average: 0.77, 0.79, 0.69

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -111,7 +111,7 @@ static void parse_elf(){
     Assert(fread(&elf_sym[i], 1, elf_shdr_symtab.sh_entsize, fp) == elf_shdr_symtab.sh_entsize, \
       "Failed to read '%s' symtab[%d]", elf_file, i);
     
-    Assert(fseek(fp, elf_shdr_strtab.sh_offset + elf_sym[12].st_name + 1, SEEK_SET) != -1, \
+    Assert(fseek(fp, elf_shdr_strtab.sh_offset + elf_sym[i].st_name + 1, SEEK_SET) != -1, \
       "Failed to read '%s' strtab", elf_file);
     char str_buf;
     while((str_buf = fgetc(fp)) != EOF){
