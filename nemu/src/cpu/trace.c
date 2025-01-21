1db73eba1453b2c22799c99fdee95b1b63d8dded
tracer-ysyx <tracer@ysyx.org>
2025-01-21 18:38:02 +0800

    > compile NEMU ysyx_22040000 杨羽童 Linux tonglewis-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:38:02 up  5:40,  3 users,  load average: 0.13, 0.16, 0.25

--- nemu/src/cpu/trace.c
+++ nemu/src/cpu/trace.c
@@ -155,7 +155,8 @@ void ftrace(Decode *s){
               printf("ret");
               funcall_time--;
               printf("[%s]\n", read_sym_str(sym_off_prev));
-            }else if(funcall_name_stack[funcall_time - 2] != sym_name){
+            }
+            if(funcall_name_stack[funcall_time - 2] != sym_name){
               /* Call */
               funcall_name_stack[funcall_time] = sym_name;
               funcall_time++;
