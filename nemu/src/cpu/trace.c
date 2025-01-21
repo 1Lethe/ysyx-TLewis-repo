4f16371a4933563202a9b7a2758769135498f95b
tracer-ysyx <tracer@ysyx.org>
2025-01-21 15:08:23 +0800

    > compile NEMU ysyx_22040000 杨羽童 Linux tonglewis-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:08:23 up  2:10,  3 users,  load average: 0.48, 0.72, 0.59

--- nemu/src/cpu/trace.c
+++ nemu/src/cpu/trace.c
@@ -16,7 +16,7 @@ extern Elf32_Shdr shdr_symtab;
 void iring_display(void){
   for(int i = 0; i < IRING_BUF_SIZE - 1; i++){
     if(iringbuf[i] == NULL) continue;
-    printf("%s", iringbuf[3]);
+    printf("%s", iringbuf[i]);
     if(i != iring_index - 1){
       printf("  %d  %d  \n", i, iring_index);
     }else{
