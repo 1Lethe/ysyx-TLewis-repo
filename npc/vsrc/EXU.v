4a951f2a07ff861f34e7160cd21c978850f68407
Tlewis <farawaylethe@163.com>
2025-01-26 11:10:09 +0800

    NPC complete auipc and lui

--- npc/vsrc/EXU.v
+++ npc/vsrc/EXU.v
@@ -1,42 +1,70 @@
 import "DPI-C" function void halt ();
+`include "define/exu_command.v"
 
-module ysyx_24120013_EXU #(ADDR_WIDTH = 5, DATA_WIDTH = 32)(
+module ysyx_24120013_EXU #(COMMAND_WIDTH = 5, ADDR_WIDTH = 5, DATA_WIDTH = 32)(
         input clk,
         input rst,
-        input [31:0] imm,
         input [DATA_WIDTH-1:0] src1,
         input [DATA_WIDTH-1:0] src2,
         input [ADDR_WIDTH-1:0] des_addr,
-        input [1:0] command,
+        input [COMMAND_WIDTH-1:0] command,
+        input pc_jump_flag,
 
         output reg EXU_wen,
         output reg [ADDR_WIDTH-1:0] EXU_waddr,
-        output reg [DATA_WIDTH-1:0] EXU_wdata
+        output reg [DATA_WIDTH-1:0] EXU_wdata,
+        output reg EXU_pc_jump_val,
+        output reg EXU_pc_jump_en
+        
     );
 
     always @(*) begin
         if(des_addr == 0) begin
             EXU_wen = 0;
             EXU_waddr = 0;
+            EXU_wdata = 0;
         end
         else begin
             EXU_wen = 1;
             EXU_waddr = des_addr;
+            EXU_wdata = alu_result;
         end
     end
 
-    always @(*) begin
-        case (command)
-            2'b01 :
-                EXU_wdata = src1 + imm;
-            default :
-                EXU_wdata = {DATA_WIDTH{1'b0}};
-        endcase
-    end
+always @(*) begin
+    case (command)
+        `ysyx_24120013_ADD : 
+            alu_command = `ysyx_24120013_ADD;
+        `ysyx_24120013_EQU : 
+            alu_command = `ysyx_24120013_EQU;
+        default : 
+            alu_command = {COMMAND_WIDTH{1'b0}};
+    endcase
+end
 
     always @(*) begin
-        if(command == 2'b11)
+        if(command == `ysyx_24120013_HALT)
             halt();
     end
+
+    wire [DATA_WIDTH-1:0] alu_src1;
+    wire [DATA_WIDTH-1:0] alu_src2;
+    reg [COMMAND_WIDTH-1:0] alu_command;
+    reg [DATA_WIDTH-1:0] alu_result;
+
+    assign alu_src1 = src1;
+    assign alu_src2 = src2;
+
+ysyx_24120013_alu #(
+    .COMMAND_WIDTH(COMMAND_WIDTH),
+    .ADDR_WIDTH(ADDR_WIDTH),
+    .DATA_WIDTH(DATA_WIDTH)
+)u_ysyx_24120013_alu(
+    .src1       	(alu_src1        ),
+    .src2       	(alu_src2        ),
+    .command    	(alu_command     ),
+    .alu_result 	(alu_result      )
+);
+
 
 endmodule
--- npc/vsrc/IDU.v
+++ npc/vsrc/IDU.v
@@ -1,65 +1,99 @@
-module ysyx_24120013_IDU #(COMMAND_WIDTH = 2, ADDR_WIDTH = 5, DATA_WIDTH = 32)(
+`include "define/exu_command.v"
+
+module ysyx_24120013_IDU #(COMMAND_WIDTH = 4, ADDR_WIDTH = 5, DATA_WIDTH = 32)(
         input clk,
         input rst,
-        input [31:0] inst,
+        input [DATA_WIDTH-1:0] inst,
+        input [DATA_WIDTH-1:0] pc,
         input [DATA_WIDTH-1:0] rdata1,
         input [DATA_WIDTH-1:0] rdata2,
-
         output wire [ADDR_WIDTH-1:0] IDU_raddr1,
         output wire [ADDR_WIDTH-1:0] IDU_raddr2,
 
-        output wire [DATA_WIDTH-1:0] IDU_src1,
-        output wire [DATA_WIDTH-1:0] IDU_src2,
+        output reg [DATA_WIDTH-1:0] IDU_src1,
+        output reg [DATA_WIDTH-1:0] IDU_src2,
         output wire [ADDR_WIDTH-1:0] IDU_des,
-        output reg [31:0] IDU_imm,
-        output reg [1:0] IDU_command
+        output reg [COMMAND_WIDTH-1:0] IDU_command
     );
 
-    parameter R_TYPE = 6'b000001;
-    parameter I_TYPE = 6'b000010;
-    parameter S_TYPE = 6'b000100;
-    parameter B_TYPE = 6'b001000;
-    parameter U_TYPE = 6'b010000;
-    parameter J_TYPE = 6'b100000;
-    parameter N_TYPE = 6'b000000;
+    parameter IMM_I = 6'b000001;
+    parameter IMM_U = 6'b000010;
+    parameter IMM_S = 6'b000100;
+    parameter IMM_J = 6'b001000;
+    parameter IMM_R = 6'b010000;
+    parameter IMM_B = 6'b100000;
+    parameter IMM_N = 6'b000000;
 
+    wire [2:0] funct3;
     wire [6:0] opcode;
 
+    reg wren_en;
     reg [5:0] imm_type;
+    reg [DATA_WIDTH-1:0] imm;
 
     assign opcode = inst[6:0];
+    assign funct3 = inst[14:12]; 
+
     assign IDU_raddr1 = inst[19:15];
     assign IDU_raddr2 = inst[24:20];
-    assign IDU_des = inst[11:7];
-    assign IDU_src1 = rdata1;
-    assign IDU_src2 = rdata2;
+
+    assign IDU_des = (wren_en == 1'b1) ? inst[11:7] : {ADDR_WIDTH{1'b0}};
 
     always @(*) begin
-        case (opcode)
-            7'b0010011 :
-                imm_type = I_TYPE;
-            default :
-                imm_type = N_TYPE;
-        endcase
-    end
-
-    always @(*) begin
-        case (imm_type)
-            I_TYPE :
-                IDU_imm = {{20{inst[31]}},inst[31:20]};
-            default :
-                IDU_imm = 32'b0;
+        case(imm_type)
+            IMM_I : imm = {{20{inst[31]}}, inst[31:20]};
+            IMM_U : imm = {inst[31:12],{12{1'b0}}};
+            IMM_N : imm = {DATA_WIDTH{1'b0}};
+            default : imm = {DATA_WIDTH{1'b0}};
         endcase
     end
 
     always @(*) begin
         case(opcode)
-            7'b0010011 :
-                IDU_command = 2'b01;
-            7'b1110011 :
-                IDU_command = 2'b11;
-            default :
-                IDU_command = 2'b00;
+            7'b00100_11 : begin // addi
+                if(funct3 == 3'b000) begin
+                    imm_type = IMM_I;
+                    IDU_command = `ysyx_24120013_ADD;
+                    IDU_src1 = imm;
+                    IDU_src2 = rdata1;
+                    wren_en = 1'b1;
+                end
+                else begin
+                    imm_type = 0;
+                    IDU_command = {COMMAND_WIDTH{1'b0}};
+                    IDU_src1 = 0;
+                    IDU_src2 = 0;
+                    wren_en = 0;
+                end
+            end
+            7'b01101_11 : begin // lui
+                imm_type = IMM_U;
+                IDU_command = `ysyx_24120013_EQU;
+                IDU_src1 = imm;
+                IDU_src2 = 0;
+                wren_en = 1'b1;
+            end
+            7'b00101_11 : begin // auipc
+                imm_type = IMM_U;
+                IDU_command = `ysyx_24120013_ADD;
+                IDU_src1 = pc;
+                IDU_src2 = imm;
+                wren_en = 1'b1;
+            end
+            7'b11100_11 : begin // ebreak
+                imm_type = IMM_N;
+                IDU_command = `ysyx_24120013_HALT;
+                IDU_src1 = 0;
+                IDU_src2 = 0;
+                wren_en = 0;
+            end
+            default : begin // default
+                imm_type = 0;
+                IDU_command = {COMMAND_WIDTH{1'b0}};
+                IDU_src1 = 0;
+                IDU_src2 = 0;
+                wren_en = 0;
+            end
         endcase
     end
 
