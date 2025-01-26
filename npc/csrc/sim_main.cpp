87fba652428cdf5d724dd259b2fe0372c09e523c
Tlewis <farawaylethe@163.com>
2025-01-26 11:09:47 +0800

    NPC complete make run npc

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -10,6 +10,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <getopt.h>
+
+char *log_file = NULL;
+char *diff_so_file = NULL;
+char *img_file = NULL;
+int difftest_port = 1234;
 
 int sim_time = SIM_TIME_MAX;
 
@@ -30,7 +36,7 @@ void sim_init(int argc, char** argv){
     tfp = new VerilatedFstC;
     contextp->traceEverOn(true);
     SIM_MODULE_NAME->trace(tfp, 99);  // Trace 99 levels of hierarchy (or see below)
-    tfp->open("wave/wave.fst");
+    tfp->open("/home/tonglewis/ysyx-workbench/npc/wave/wave.fst");
 }
 
 void dump_wave(SIM_MODULE* top){
@@ -52,11 +58,42 @@ void reset(SIM_MODULE* top, int n){
 }
 #endif
 
+static int parse_args(int argc, char *argv[]) {
+    const struct option table[] = {
+        {"log"      , required_argument, NULL, 'l'},
+        {"diff"     , required_argument, NULL, 'd'},
+        {"port"     , required_argument, NULL, 'p'},
+        {"help"     , no_argument      , NULL, 'h'},
+        {0          , 0                , NULL,  0 },
+    };
+    int o;
+    while ( (o = getopt_long(argc, argv, "-bhl:d:p:", table, NULL)) != -1) {
+    switch (o) {
+        case 'p': sscanf(optarg, "%d", &difftest_port); break;
+        case 'l': log_file = optarg; break;
+        case 'd': diff_so_file = optarg; break;
+        case 1: img_file = optarg; return 0;
+        default:
+            printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
+            printf("\t-l,--log=FILE           output log to FILE\n");
+            printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
+            printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
+            printf("\n");
+            exit(0);
+        }
+    }
+    return 0;
+}
+
 int main(int argc, char** argv) {                                      
+
+    parse_args(argc, argv);
     
     sim_init(argc, argv);
 
     reset(SIM_MODULE_NAME, 1);
+
+    load_img();
 
 #ifdef USE_TESTBENCH
     while(!contextp->gotFinish()){   
