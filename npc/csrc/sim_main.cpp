03525499b73acdf7654acceff4a5befcae2811fe
tracer-ysyx <tracer@ysyx.org>
2025-02-04 13:02:43 +0800

    > sim RTL ysyx_22040000 杨羽童 Linux tonglewis-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:02:43 up 45 min,  3 users,  load average: 0.75, 0.75, 0.66

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -26,7 +26,6 @@ SIM_MODULE* SIM_MODULE_NAME;
 void halt(void){
     printf("\nProgram Finished at clock time %d.\n", SIM_TIME_MAX - sim_time);
     SIM_MODULE_NAME->clk = 0;SIM_MODULE_NAME->eval();dump_wave(top);
-    SIM_MODULE_NAME->clk = 1;SIM_MODULE_NAME->eval();dump_wave(top);
     sim_time = 0;
 }
 
