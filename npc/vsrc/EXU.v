11e619ec155c1f9da265242274107c3a158cf186
tracer-ysyx <tracer@ysyx.org>
2025-02-04 17:05:25 +0800

    > sim RTL ysyx_22040000 杨羽童 Linux tonglewis-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:05:25 up  4:47,  3 users,  load average: 3.59, 4.08, 4.12

--- npc/vsrc/EXU.v
+++ npc/vsrc/EXU.v
@@ -23,9 +23,9 @@ module ysyx_24120013_EXU #(ADDR_WIDTH = 5, DATA_WIDTH = 32)(
         output wire [DATA_WIDTH-1:0] branch_jmp_pc
     );
 
-    assign reg_wen = (des_addr == 0) ? 1'b0 : 1'b1;
-    assign reg_waddr = (des_addr == 0) ? {ADDR_WIDTH{1'b0}} : des_addr;
-    assign reg_wdata = (des_addr == 0) ? {DATA_WIDTH{1'b0}} : alu_result;
+    assign reg_wen = (alu_des_addr == 0) ? 1'b0 : 1'b1;
+    assign reg_waddr = (alu_des_addr == 0) ? {ADDR_WIDTH{1'b0}} : des_addr;
+    assign reg_wdata = (alu_des_addr == 0) ? {DATA_WIDTH{1'b0}} : alu_result;
 
     always @(*) begin
         if(break_ctrl) halt();
