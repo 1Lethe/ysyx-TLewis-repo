63eacbf961b6852594d545d46724eb17a64d0acc
tracer-ysyx <tracer@ysyx.org>
2025-02-04 12:39:29 +0800

    > sim RTL ysyx_22040000 杨羽童 Linux tonglewis-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:39:29 up 21 min,  3 users,  load average: 1.16, 0.86, 0.63

--- npc/csrc/npcsrc/memory.c
+++ npc/csrc/npcsrc/memory.c
@@ -2,16 +2,19 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <assert.h>
+#include <string.h>
 #include "memory.h"
 
 extern char *img_file;
 
-uint8_t pmem[MAX_MEMORY] __attribute((aligned(4096))) = {
+uint8_t pmem[MAX_MEMORY] __attribute((aligned(4096))) = {};
+
+static const uint32_t buildin_img[] = {
     0x000400b7, // lui x1,64 (64,0)
     0x01008113, // addi x2,x1,16 (64,80)
     0x000ff197, // auipc x3,255
     0x00100073, // ebreak
-};
+}
 
 uint8_t* guest_to_host(uint32_t paddr) { return pmem + paddr - RESET_VECTOR; }
 uint32_t host_to_guest(uint8_t *haddr) { return haddr - pmem + RESET_VECTOR; }
@@ -30,6 +33,7 @@ void mem_out_of_bound(uint32_t addr){
 long load_img() {
     if(img_file == NULL){
         printf("No image is given.Use the default build-in image.\n");
+        memcpy(guest_to_host(RESET_VECTOR), buildin_img, sizeof(buildin_img));
         return 4096;
     }
 
