725e85b0da6cc4e151f5486b87e1c8fd679cc389
tracer-ysyx <tracer@ysyx.org>
2025-02-07 01:02:03 +0800

    > sim RTL ysyx_22040000 杨羽童 Linux tonglewis-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:02:03 up  4:52,  3 users,  load average: 0.36, 0.44, 0.45

--- npc/csrc/include/sdb.h
+++ npc/csrc/include/sdb.h
@@ -3,13 +3,13 @@
 
 #include "Vysyx_24120013_top.h"
 #include "verilated.h"
-/*
+
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
-*/
+
 #include "macro.h"
 #include "sim_main.h"
 #include "cpu-exec.h"
