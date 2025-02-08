f40e1bf71078138b6c650c0b9029b73efefb0981
tracer-ysyx <tracer@ysyx.org>
2025-02-08 12:30:31 +0800

    > sim RTL ysyx_22040000 杨羽童 Linux tonglewis-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:30:31 up 26 min,  3 users,  load average: 0.46, 0.78, 0.68

--- npc/csrc/include/debug.h
+++ npc/csrc/include/debug.h
@@ -19,6 +19,7 @@
 #include <stdio.h>
 #include <utils.h>
 #include <macro.h>
+#include "trace.h"
 
 #define Log(format, ...) \
     _Log(ANSI_FMT("[%s:%d %s] " format, ANSI_FG_BLUE) "\n", \
@@ -28,6 +29,9 @@
 #define Assert(cond, format, ...) \
   do { \
     if (!(cond)) { \
+      MUXDEF(CONFIG_TARGET_AM, printf(ANSI_FMT(format, ANSI_FG_RED) "\n", ## __VA_ARGS__), \
+        (fflush(stdout), fprintf(stderr, ANSI_FMT(format, ANSI_FG_RED) "\n", ##  __VA_ARGS__))); \
+      IFNDEF(CONFIG_TARGET_AM, /*extern FILE* log_fp; fflush(log_fp)*/); \
       extern void assert_fail_msg(); \
       assert_fail_msg(); \
       assert(cond); \
