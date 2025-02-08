3b299635c6abc3067015e985d3c78c496960a260
tracer-ysyx <tracer@ysyx.org>
2025-02-08 13:30:52 +0800

    > sim RTL ysyx_22040000 杨羽童 Linux tonglewis-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:30:52 up  1:26,  3 users,  load average: 0.15, 0.42, 0.27

--- npc/csrc/include/debug.h
+++ npc/csrc/include/debug.h
@@ -29,6 +29,7 @@
 #define Assert(cond, format, ...) \
   do { \
     if (!(cond)) { \
+      printf(ANSI_FMT(format, ANSI_FG_RED) "\n", ## __VA_ARGS__); \
       assert_fail_msg(); \
       assert(cond); \
     } \
--- npc/csrc/monitor/monitor.c
+++ npc/csrc/monitor/monitor.c
@@ -40,10 +40,10 @@ static int parse_args(int argc, char *argv[]) {
 }
 
 long load_img() {
+        Assert(0, "test");
     if(img_file == NULL){
         printf("No image is given.Use the default build-in image.\n");
         cpy_buildin_img();
-        Assert(0, "test");
         return 4096;
     }
 
