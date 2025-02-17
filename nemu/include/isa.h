ecd3be496c840460a627bc29e27b07d47a7ad709
Tlewis <farawaylethe@163.com>
2025-02-17 17:18:25 +0800

    NEMU add checkpc in difftest , update difftest fail msg

--- nemu/include/isa.h
+++ nemu/include/isa.h
@@ -31,6 +31,7 @@ void init_isa();
 // reg
 extern CPU_state cpu;
 void isa_reg_display();
+void diff_isa_reg_display(CPU_state *ref);
 word_t isa_reg_str2val(const char *name, bool *success);
 
 // exec
