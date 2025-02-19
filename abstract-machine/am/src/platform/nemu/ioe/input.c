0a8d6a754b7032f3b9f90e8abd8f2745d49dc92a
Tlewis <farawaylethe@163.com>
2025-02-19 21:38:07 +0800

    PA2 complete keyboard

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,6 +4,11 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  uint32_t key_read = inl(KBD_ADDR);
+  kbd->keycode = (uint8_t)key_read;
+  if((key_read & KEYDOWN_MASK) >> 15 == 1){
+    kbd->keydown = true;
+  }else{
+    kbd->keydown = false;
+  }
 }
