e79af2cbc032bc38a2c5113ea7959a54fcebf4cb
Tlewis <farawaylethe@163.com>
2025-03-13 15:55:45 +0800

    NPC AM complete yield

--- abstract-machine/am/src/riscv/npc/cte.c
+++ abstract-machine/am/src/riscv/npc/cte.c
@@ -8,6 +8,7 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
+      case 0xb : ev.event = EVENT_YIELD; c->mepc += 0x4; break;
       default: ev.event = EVENT_ERROR; break;
     }
 
@@ -31,7 +32,13 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 }
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-  return NULL;
+  Context *c = (Context *)kstack.end - 1;
+
+  c->gpr[1] = (uintptr_t)(c + 1); // reg sp
+  c->gpr[9] = (uintptr_t)arg;     // reg a0
+  c->mepc = (uintptr_t)entry;
+  c->mstatus = 0x1800;
+  return c;
 }
 
 void yield() {
