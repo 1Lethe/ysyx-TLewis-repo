09c607077f577733d14685040e3d6da76a668c9e
Tlewis <farawaylethe@163.com>
2025-03-13 16:03:41 +0800

    NPC fix default config

--- npc/csrc/include/config.h
+++ npc/csrc/include/config.h
@@ -5,12 +5,12 @@
 //#define USE_TESTBENCH
 
 #define EN_ITRACE 1
-#define EN_MTRACE 1
-#define EN_FTRACE 1
-#define EN_DTRACE 1
+//#define EN_MTRACE 1
+//#define EN_FTRACE 1
+//#define EN_DTRACE 1
 #define EN_DIFFTEST 1
 #define EN_DEVICE 1
-#define EN_DUMP_WAVE 1
+//#define EN_DUMP_WAVE 1
 
 
 #endif
